d6b0dfa3bfab2142ffa9dfb915eb940726895abd
Ranur-react <rahmatnur845@gmail.com>
2021-01-27 22:38:57 +0700

    upload Webiste dari amaik

--- webabsen/application/controllers/AuthApp.php
+++ webabsen/application/controllers/AuthApp.php
@@ -18,15 +18,6 @@ class AuthApp extends CI_Controller
         }
     }
 
-
-
-
-
-
-
-
-
-
     public function Usernamecek()
     {
         $usrname = trim($this->input->post('username'));
@@ -49,15 +40,12 @@ class AuthApp extends CI_Controller
          $obj = json_decode($js,true);
          $usrname = $obj['username'];
          $password = $obj['password'];
-
         
 
         if ($this->Mauth->check_user($usrname)->num_rows()!=0 ) {
            if ($this->Mauth->check_pass($usrname, $password)->num_rows() !=0 ) {
                  $data=$this->Mauth->check_pass($usrname, $password)->row_array();
                  $dk=$this->Mdata_karyawan->shows($data['kode_user']);
-                
-                                $json['Data'] = $data;
                                 $json['IDkaryawan'] = $data['kode_user'];
                                  if (!$dk==null) $json['namakaryawan'] = $dk['nama_karyawan']; 
                                     else $json['namakaryawan'] = "Admin";
@@ -77,73 +65,6 @@ class AuthApp extends CI_Controller
         echo json_encode($json);
     }
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-    public function validate()
-    {
-        $usrname = trim($this->input->post('username'));
-        $password = trim($this->input->post('password'));
-        $check_user = $this->Mauth->check_user($usrname);
-        $check_pass = $this->Mauth->check_pass($usrname, $password);
-        $this->form_validation->set_rules('username', 'Username', 'callback_username_check[' . $check_user->num_rows() . ']');
-        $this->form_validation->set_rules('password', 'Password', 'callback_password_check[' . $check_pass->num_rows() . ']');
-        if ($this->form_validation->run() == TRUE) {
-            $post = $this->input->post(null, TRUE);
-            $value = $this->Mauth->validate($post);
-            $this->session->set_userdata('masuk', TRUE);
-            if ($this->session->userdata('masuk') == TRUE) {
-                $this->session->set_userdata('status_login', TRUE);
-                $this->session->set_userdata('kode', $value['id_user']);
-                $this->session->set_flashdata('pesan', sukses('Berhasli Login.'));
-            } else {
-                $this->session->session_destroy();
-            }
-            $json['status'] = TRUE;
-        } else {
-            $json['pesan'] = $this->form_validation->error_array();
-        }
-        echo json_encode($json);
-    }
-    public function username_check($username, $recordCount)
-    {
-        if ($username == null) {
-            $this->form_validation->set_message('username_check', 'Username is required.');
-            return false;
-        } else if ($recordCount == 0) {
-            $this->form_validation->set_message('username_check', 'Username is not correct.');
-            return FALSE;
-        } else {
-
-            return true;
-        }
-    }
-    public function password_check($password, $recordCount)
-    {
-        if ($password == null) {
-            $this->form_validation->set_message('password_check', 'Password is required.');
-            return false;
-        } else if ($recordCount == 0) {
-            $this->form_validation->set_message('password_check', 'Password is not correct.');
-            return FALSE;
-        } else {
-
-            return true;
-        }
-    }
     public function logout()
     {
         //$this->session->sess_destroy();
