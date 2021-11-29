eee03f3047267bd8ea84e378bad1c6812bda7f25
Ranur-react <rahmatnur845@gmail.com>
2021-11-29 20:58:56 +0700

    update

--- absensigunungmasp1/View/Masuk/Capture/index.js
+++ absensigunungmasp1/View/Masuk/Capture/index.js
@@ -16,10 +16,10 @@ import {
   Button
 } from 'react-native';
 //library
-import {launchCamera, launchImageLibrary} from 'react-native-image-picker';
+import { launchCamera, launchImageLibrary } from 'react-native-image-picker';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 import Geolocation from '@react-native-community/geolocation';
-import {getDistance, getPreciseDistance} from 'geolib';
+import { getDistance, getPreciseDistance } from 'geolib';
 
 //asset
 import Svgicon from './../../../assets/icons/Svgicon';
@@ -32,11 +32,11 @@ import Camera from './Camera';
 //end-class
 
 //var/data declarated
-const {width:WIDTH} =Dimensions.get('window');
-const {height:HEIGHT} =Dimensions.get('window');
+const { width: WIDTH } = Dimensions.get('window');
+const { height: HEIGHT } = Dimensions.get('window');
 
 
-const storeDataString = async (key,value) => {
+const storeDataString = async (key, value) => {
   try {
     await AsyncStorage.setItem(key, value);
   } catch (e) {
@@ -63,14 +63,14 @@ export default class MyComponent extends Component {
   selectFile = () => {
     var options = {
       mediaType: 'photo',
-      maxWidth:1000,
-      maxHeight:1000,
-      saveToPhotos:true,
-      includeBase64:true
+      maxWidth: 1000,
+      maxHeight: 1000,
+      saveToPhotos: true,
+      includeBase64: true
     };
-  launchCamera(options, res => {
-      if (!res.didCancel){
-        storeDataString('Ft',res.uri);
+    launchCamera(options, res => {
+      if (!res.didCancel) {
+        storeDataString('Ft', res.uri);
         const jsonValue = JSON.stringify(res);
         storeDataJson(jsonValue);
         this.props.navigation.navigate('MasukAfterCapture');
@@ -81,19 +81,19 @@ export default class MyComponent extends Component {
   render() {
 
     return (
-      <View  style={styles.Backcontainer}>
-        <View  style={styles.container}>
-        <View  style={styles.Textcontainer}>
-            <TouchableOpacity style={styles.backButton} onPress={() =>this.props.navigation.goBack()} >
+      <View style={styles.Backcontainer}>
+        <View style={styles.container}>
+          <View style={styles.Textcontainer}>
+            <TouchableOpacity style={styles.backButton} onPress={() => this.props.navigation.goBack()} >
               <Svgicon key={999} name="Back" color="black" />
             </TouchableOpacity>
-            <Deskripsiabsen />
+            <Deskripsiabsen key={567} />
             <TouchableOpacity onPress={this.selectFile}>
-              <Camera key={99} />
+              <Camera key={95469} />
             </TouchableOpacity>
+          </View>
         </View>
-        </View>
-        </View>
+      </View>
     );
   }
 }
@@ -103,28 +103,28 @@ const styles = StyleSheet.create({
     flex: 1,
     alignItems: 'center',
   },
-  backButton:{
-    backgroundColor:'rgba(50,50,50,0)',
-    borderRadius:50,
-    top:-15,
-    left:-15,
-    width:70,
-    height:70,
-    alignItems:'center',
-    justifyContent:'center',
+  backButton: {
+    backgroundColor: 'rgba(50,50,50,0)',
+    borderRadius: 50,
+    top: -15,
+    left: -15,
+    width: 70,
+    height: 70,
+    alignItems: 'center',
+    justifyContent: 'center',
   }
   ,
   container: {
-    top:20,
-    width:WIDTH-30,
-    alignItems:'flex-start',
+    top: 20,
+    width: WIDTH - 30,
+    alignItems: 'flex-start',
     // backgroundColor:'grey',
     // borderColor:'black',
     // borderWidth:1,
   },
-  Textcontainer:{
-    width:'100%',
-    paddingBottom:20
+  Textcontainer: {
+    width: '100%',
+    paddingBottom: 20
     // backgroundColor:'grey'
   }
 });
