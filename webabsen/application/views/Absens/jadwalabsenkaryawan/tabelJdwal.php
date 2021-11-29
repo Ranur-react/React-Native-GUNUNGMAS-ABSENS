61480a3b4a537d7fbb6de27fbf59da1deac704e3
Ranur-react <rahmatnur845@gmail.com>
2021-11-29 20:27:55 +0700

    hilangkan edit pada jadwal karyawan

--- webabsen/application/views/Absens/jadwalabsenkaryawan/tabelJdwal.php
+++ webabsen/application/views/Absens/jadwalabsenkaryawan/tabelJdwal.php
@@ -8,7 +8,7 @@
 								<td><?= $d['lokasi'] ?></td>
 								<td><?= $d['nama_karyawan'] ?></td>
 								<td class="text-center" width="60px">
-									<a href="javascript:void(0)" onclick="edit('<?= $d['id_jadwal'] ?>')"><i class="icon-pencil7 text-green" data-toggle="tooltip" data-original-title="Edit Data"></i></a>
+									<!-- <a href="javascript:void(0)" onclick="edit('<?= $d['id_jadwal'] ?>')"><i class="icon-pencil7 text-green" data-toggle="tooltip" data-original-title="Edit Data"></i></a> -->
 									<a href="<?= site_url('Absens/JadwalAbsenKaryawan/destroy/' . $d['id_jadwal']) ?>" onclick="return confirm('Yakin akan hapus data ini ?');"><i class="icon-trash text-red" data-toggle="tooltip" data-original-title="Hapus Data"></i></a>
 								</td>
 							</tr>
