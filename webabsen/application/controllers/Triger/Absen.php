5599bab9e17007a5cd37c5234a3dc76d14d020bd
Ranur-react <rahmatnur845@gmail.com>
2021-12-09 21:27:05 +0700

    absen report crash

--- webabsen/application/controllers/Triger/Absen.php
+++ webabsen/application/controllers/Triger/Absen.php
@@ -56,7 +56,7 @@ class Absen extends CI_Controller
 		if (move_uploaded_file($_FILES['imagos']['tmp_name'], $URI)) {
 			if ($_POST['StatusAbsen'] == "Masuk") {
 				//Input Data Absen Masuk-----------
-				// $this->db->query("INSERT INTO `absen_masuk` VALUES ('$ID', '$IDKARYAWAN', '$IDJADWAL','$JAM',NOW(),'$LA','$LO','$FOTO','HADIR'); ");
+				$this->db->query("INSERT INTO `absen_masuk` VALUES ('$ID', '$IDKARYAWAN', '$IDJADWAL','$JAM',NOW(),'$LA','$LO','$FOTO','HADIR'); ");
 
 				// $this->db->query("UPDATE `detail_jadwal` SET `status_kehadiran` = 'm' , `status_displin` = '$Displin' WHERE `id_jadwal_detail` = '$IDJADWAL' AND `id_karyawan_detail` = '$IDKARYAWAN' AND `tanggal` = DATE_FORMAT(NOW(), '%Y-%m-%d');");
 
