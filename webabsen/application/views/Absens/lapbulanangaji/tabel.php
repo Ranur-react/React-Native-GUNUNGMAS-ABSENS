de8efa5e08dedf1d381a55fb11f340e0a39e389a
Ranur-react <rahmatnur845@gmail.com>
2021-12-12 21:27:41 +0700

    Query Tunjangan

--- webabsen/application/views/Absens/lapbulanangaji/tabel.php
+++ webabsen/application/views/Absens/lapbulanangaji/tabel.php
@@ -1,13 +1,17 @@
 <?php $no = 1;
-						foreach ($dataVar as $d) { ?>
-							<tr>
-								<td class="text-center" width="40px"><?= $no . '.'; ?></td>
-								<td><?= $d['nama_karyawan'] ?></td>
-								<td><?= $d['lokasi'] ?></td>
-								<td><?= $d['hadir'] ?></td>
-								<td><?= $d['sakit'] ?></td>
-								<td><?= $d['izin'] ?></td> 
-								<!-- <td><?= number_format(($d['hadir']/30)*100, 0)."%"; ?></td>  -->
-							</tr>
-						<?php $no++;
-						} ?>
+foreach ($dataVar as $d) { ?>
+	<tr>
+		<td class="text-center" width="40px"><?= $no . '.'; ?></td>
+		<td><?= $d['nama_karyawan'] ?></td>
+		<td><?= $d['lokasi'] ?></td>
+		<td><?= $d['hadir'] ?></td>
+		<td><?= $d['sakit'] ?></td>
+		<td><?= $d['izin'] ?></td>
+		<td><?= $d['izin'] ?></td>
+		<td><?= $d['izin'] ?></td>
+		<td><?= $d['izin'] ?></td>
+		<td><?= $d['izin'] ?></td>
+		<!-- <td><?= number_format(($d['hadir'] / 30) * 100, 0) . "%"; ?></td>  -->
+	</tr>
+<?php $no++;
+} ?>
