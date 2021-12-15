1ae0e4de536218a660a3e174d63e7f9a84272476
Ranur-react <rahmatnur845@gmail.com>
2021-12-15 22:39:27 +0700

    Parameter Laporan bulan diperbaiki

--- webabsen/application/models/Absens/Mlap_bulanan.php
+++ webabsen/application/models/Absens/Mlap_bulanan.php
@@ -30,8 +30,9 @@ class Mlap_bulanan extends CI_Model
 	$a=$v.' month';
 	$date = date_create('2020-12-01');
 	date_add($date, date_interval_create_from_date_string($a));
-	$d=date_format($date, 'm');
-		return $this->db->query("SELECT `id_karyawan`,`nama_karyawan`, `lokasi`,
+	$d=date_format($date, 'Y-m');
+		return $this->db->query("
+		SELECT `id_karyawan`,`nama_karyawan`, `lokasi`,
 				 
 				(SELECT COUNT(`status_kehadiran`) FROM `detail_jadwal` WHERE `id_karyawan`=`id_karyawan_detail` AND `status_kehadiran` != 'null'AND `status_kehadiran` != '0' )
 				 AS hadir,
@@ -43,13 +44,18 @@ class Mlap_bulanan extends CI_Model
 				 AS izin,
 				 (SELECT COUNT(`status_displin`) FROM `detail_jadwal` WHERE `id_karyawan`=`id_karyawan_detail` AND `status_kehadiran` != 'null'AND `status_kehadiran` != '0')
 				 AS status_displin,
-				 tanggal
+				 tanggal,
+				`tb_jabatan`.`gapok` AS gapok,
+				`tb_jabatan`.`tunjangan_disiplin` AS tdisplin,
+				`tb_jabatan`.`potongan_disiplin` AS pdisplin
 				 FROM `jadwal_absen_karyawan` 
 				JOIN  `detail_jadwal` ON `id_jadwal_detail`=`id_jadwal`
 				JOIN `karyawan` ON `id_karyawan` =`id_karyawan_detail`
 				JOIN `set_lokasi` ON `id_set_lokasi` =`id_lokasi_absensi`
+				JOIN `tb_jabatan` ON `tb_jabatan`.`id_jabatan`=karyawan.`jabatan_id`
 				WHERE  MONTH(tanggal)='$d'
-				GROUP BY `id_karyawan`;")->result_array();
+				GROUP BY `id_karyawan`;
+		")->result_array();
 	}
 
 	public function tampildata()
