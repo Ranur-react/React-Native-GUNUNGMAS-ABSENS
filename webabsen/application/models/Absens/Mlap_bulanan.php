ee6a77ae41f93cac71906ebd224c13f671105c39
Ranur-react <rahmatnur845@gmail.com>
2021-12-15 22:41:33 +0700

    Parameter Laporan bulan diperbaiki

--- webabsen/application/models/Absens/Mlap_bulanan.php
+++ webabsen/application/models/Absens/Mlap_bulanan.php
@@ -52,7 +52,7 @@ class Mlap_bulanan extends CI_Model
 				JOIN `karyawan` ON `id_karyawan` =`id_karyawan_detail`
 				JOIN `set_lokasi` ON `id_set_lokasi` =`id_lokasi_absensi`
 				JOIN `tb_jabatan` ON `tb_jabatan`.`id_jabatan`=karyawan.`jabatan_id`
-				WHERE  MONTH(tanggal)='$d'
+				WHERE  MONTH(tanggal)='$v'
 				GROUP BY `id_karyawan`")->result_array();
 	}
 
