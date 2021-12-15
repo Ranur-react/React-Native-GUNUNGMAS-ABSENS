4da201a652f95590406321c82bf34b1d339dbebe
Ranur-react <rahmatnur845@gmail.com>
2021-12-15 22:36:36 +0700

    Parameter Laporan bulan diperbaiki

--- webabsen/application/models/Absens/Mlap_bulanan.php
+++ webabsen/application/models/Absens/Mlap_bulanan.php
@@ -33,23 +33,23 @@ class Mlap_bulanan extends CI_Model
 	$d=date_format($date, 'Y-m');
 		return $this->db->query("SELECT `id_karyawan`,`nama_karyawan`, `lokasi`,
 				 
-				(SELECT COUNT(`status_kehadiran`) FROM `detail_jadwal` WHERE `id_karyawan`=`id_karyawan_detail` AND `status_kehadiran` = '1' AND tanggal LIKE '%$d%')
+				(SELECT COUNT(`status_kehadiran`) FROM `detail_jadwal` WHERE `id_karyawan`=`id_karyawan_detail` AND `status_kehadiran` != 'null'AND `status_kehadiran` != '0' )
 				 AS hadir,
 
-				 (SELECT COUNT(`status_kehadiran`) FROM `detail_jadwal` WHERE `id_karyawan`=`id_karyawan_detail` AND `status_kehadiran` = 's' AND tanggal LIKE '%$d%')
+				 (SELECT COUNT(`status_kehadiran`) FROM `detail_jadwal` WHERE `id_karyawan`=`id_karyawan_detail` AND `status_kehadiran` = 's')
 				 AS sakit,
 				 
-				  (SELECT COUNT(`status_kehadiran`) FROM `detail_jadwal` WHERE `id_karyawan`=`id_karyawan_detail` AND `status_kehadiran` = 'i' AND tanggal LIKE '%$d%')
+				  (SELECT COUNT(`status_kehadiran`) FROM `detail_jadwal` WHERE `id_karyawan`=`id_karyawan_detail` AND `status_kehadiran` = 'i')
 				 AS izin,
- `tb_jabatan`.`gapok` as gapok,
- `tb_jabatan`.`tunjangan_disiplin` as tdisplin,
- `tb_jabatan`.`potongan_disiplin` as pdisplin
- FROM `jadwal_absen_karyawan` 
-JOIN  `detail_jadwal` ON `id_jadwal_detail`=`id_jadwal`
-JOIN `karyawan` ON `id_karyawan` =`id_karyawan_detail`
-JOIN `set_lokasi` ON `id_set_lokasi` =`id_lokasi_absensi`
-JOIN `tb_jabatan` ON `tb_jabatan`.`id_jabatan`=karyawan.`jabatan_id`
-GROUP BY `id_karyawan`;")->result_array();
+				 (SELECT COUNT(`status_displin`) FROM `detail_jadwal` WHERE `id_karyawan`=`id_karyawan_detail` AND `status_kehadiran` != 'null'AND `status_kehadiran` != '0')
+				 AS status_displin,
+				 tanggal
+				 FROM `jadwal_absen_karyawan` 
+				JOIN  `detail_jadwal` ON `id_jadwal_detail`=`id_jadwal`
+				JOIN `karyawan` ON `id_karyawan` =`id_karyawan_detail`
+				JOIN `set_lokasi` ON `id_set_lokasi` =`id_lokasi_absensi`
+				WHERE  MONTH(tanggal)='$d'
+				GROUP BY `id_karyawan`;")->result_array();
 	}
 
 	public function tampildata()
