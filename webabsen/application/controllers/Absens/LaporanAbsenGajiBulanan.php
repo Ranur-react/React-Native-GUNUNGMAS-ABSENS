aca6a3b7bf49327348ddc22f1178c5e33edcd37c
Rahmat Nur <rahmatnur845@gmail.com>
2021-12-25 17:29:19 +0700

    push

--- webabsen/application/controllers/Absens/LaporanAbsenGajiBulanan.php
+++ webabsen/application/controllers/Absens/LaporanAbsenGajiBulanan.php
@@ -29,7 +29,7 @@ class LaporanAbsenGajiBulanan extends CI_Controller
 		$month = $this->uri->segment(4);
 		$emp = $this->uri->segment(5);
 		$data = [
-			'data'  => $this->Mlap_bulanan->showsSlips($month,$emp),
+			'dataVar'  => $this->Mlap_bulanan->showsSlips($month,$emp),
 			'bulan' => $month,
 			
 		];
--- webabsen/application/views/Absens/lapbulanangaji/cetak.php
+++ webabsen/application/views/Absens/lapbulanangaji/cetak.php
@@ -115,11 +115,3 @@ foreach ($dataVar as $d) { ?>
 	</html>
 <?php $no++;
 } ?>
-<?php
-// $html = ob_get_contents();
-// ob_end_clean();
-// require_once('html2pdf/html2pdf.class.php');
-// $pdf = new HTML2PDF('P','A4','en');
-// $pdf->WriteHTML($html);
-// $pdf->Output('Laporan File.pdf', 'D');
-?>
