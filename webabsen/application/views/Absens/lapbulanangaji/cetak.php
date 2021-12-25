5c5c5046a860095fbaa99088749f033b64318626
Rahmat Nur <rahmatnur845@gmail.com>
2021-12-25 17:17:41 +0700

    laporan slip gaji

--- webabsen/application/views/Absens/lapbulanangaji/cetak.php
+++ webabsen/application/views/Absens/lapbulanangaji/cetak.php
@@ -1,109 +1,120 @@
 <?php //ob_start(); 
-?>
-<!DOCTYPE html>
+foreach ($dataVar as $d) { ?>
+	?>
+	<!DOCTYPE html>
 
 
 
-<html>
+	<html>
 
-<head>
-	<title>Laporan Absensi Bulanan</title>
-	<style>
-		table {
-			border-collapse: collapse;
-			table-layout: fixed;
-		}
+	<head>
+		<title>Laporan Absensi Bulanan</title>
+		<style>
+			table {
+				border-collapse: collapse;
+				table-layout: fixed;
+			}
 
-		table td {
-			word-wrap: break-word;
-			text-align: left;
-		}
-	</style>
-</head>
+			table td {
+				word-wrap: break-word;
+				text-align: left;
+			}
+		</style>
+	</head>
+	<?php
+	$tuk = 0;
+	if ($persentasHadir < 80) {
+		$tuk = 0;
+	} else {
+		$tuk = $d['tdisplin'];
+	}
+	?>
 
-<body onload="window.print()">
-	<h1 align="center">SLIP PEMBAYARAN GAJI
-		<br>LORUS CELULER
-	</h1>
-	<h3 align="center">PERIODE : <?= '1 ' . date('M Y') . ' - ' . '30 ' . date('M Y') ?></h3>
-	<table align="center" width="80%" border="0">
-		<tr>
-			<td width="50%">
-				<table align="center" width="100%" border="0">
-					<tr>
-						<td width="50%">NO.ID KARYAWAN </td>
-						<td align="right" width="50%">: 1300056</td>
-					</tr>
-					<tr>
-						<td width="50%">NAMA KARYAWAN </td>
-						<td align="right" width="50%">: IMAM</td>
-					</tr>
-					<tr>
-						<td width="50%">JABATAN</td>
-						<td align="right" width="50%">: CS</td>
-					</tr>
-					<tr>
-						<td width="50%">BANK TRANSFER</td>
-						<td align="right" width="50%">: MANDIRI</td>
-					</tr>
-					<tr>
-						<td width="50%">NOMOR REKENING</td>
-						<td align="right" width="50%">: 12312 123 312 </td>
-					</tr>
-					<tr>
-						<td width="50%">ATAS NAMA</td>
-						<td align="right" width="50%">: IMAM</td>
-					</tr>
-					<tr>
-						<td width="50%">DIBAYARKAN TANGGAL</td>
-						<td align="right" width="50%">: <?= date('D M Y')?></td>
-					</tr>
-				</table>
-			</td>
-			<td width="50%">
-				<table align="center" width="100%" border="0">
-					<tr>
-						<td width="50%">HADIR /ALFA </td>
-						<td align="right" width="50%">: 28/2</td>
-					</tr>
-					<tr>
-						<td width="50%">PERSENTASI HADIR </td>
-						<td align="right" width="50%">: 96%</td>
-					</tr>
-					<tr>
-						<td width="50%">TUNJANGAN </td>
-						<td align="right" width="50%">: Rp. 300.000</td>
-					</tr>
-					<tr>
-						<td width="50%">TERLAMBAT </td>
-						<td align="right" width="50%">: 10 X</td>
-					</tr>
-					<tr>
-						<td width="50%">POTONGAN :</td>
-						<td align="right" width="50%">: Rp. 100.000</td>
-					</tr>
-					<tr>
-						<td width="50%"></td>
-						<td align="right" width="50%">---------------------</td>
-					</tr>
-					<tr>
-						<td width="50%">GAJI DITERIMA :</td>
-						<td align="right" width="50%">: Rp. 3.2500.000</td>
-					</tr>
-				</table>
-			</td>
-		</tr>
-	</table>
-	
-	<center><br>Padang,
-		<?php echo date('d-M-y') ?>
-		<br><br><br><br>
-		<u>(......................................)</u><br>
-		<b>Owner Toko</b>
-	</center>
-</body>
+	<body onload="window.print()">
+		<h1 align="center">SLIP PEMBAYARAN GAJI
+			<br>LORUS CELULER
+		</h1>
+		<h3 align="center">PERIODE : <?= '1 ' . date('M Y') . ' - ' . '30 ' . date('M Y') ?></h3>
+		<table align="center" width="80%" border="0">
+			<tr>
+				<td width="50%">
+					<table align="center" width="100%" border="0">
+						<tr>
+							<td width="50%">NO.ID KARYAWAN </td>
+							<td align="right" width="50%">: <?= $d['id_karyawan'] ?></td>
+						</tr>
+						<tr>
+							<td width="50%">NAMA KARYAWAN </td>
+							<td align="right" width="50%">: <?= $d['nama_karyawan'] ?></td>
+						</tr>
+						<tr>
+							<td width="50%">JABATAN</td>
+							<td align="right" width="50%">: <?= $d['nama_jabatan'] ?></td>
+						</tr>
+						<tr>
+							<td width="50%">BANK TRANSFER</td>
+							<td align="right" width="50%">: MANDIRI</td>
+						</tr>
+						<tr>
+							<td width="50%">NOMOR REKENING</td>
+							<td align="right" width="50%">: 12312 123 312 </td>
+						</tr>
+						<tr>
+							<td width="50%">ATAS NAMA</td>
+							<td align="right" width="50%">: <?= $d['nama_karyawan'] ?></td>
+						</tr>
+						<tr>
+							<td width="50%">DIBAYARKAN TANGGAL</td>
+							<td align="right" width="50%">: <?= date('D M Y') ?></td>
+						</tr>
+					</table>
+				</td>
+				<td width="50%">
+					<table align="center" width="100%" border="0">
+						<tr>
+							<td width="50%">HADIR /ALFA </td>
+							<td align="right" width="50%">: <?= $d['hadir'] ?>/<?= (30 - $d['hadir']) ?></td>
+						</tr>
+						<tr>
+							<td width="50%">PERSENTASI HADIR </td>
+							<td align="right" width="50%">: <?= number_format($persentasHadir, 0) . '% ' ?></td>
+						</tr>
+						<tr>
+							<td width="50%">TUNJANGAN </td>
+							<td align="right" width="50%">: <?= $persentasHadir < 80 ? '~ kehadiran belum cukup ' : 'Rp. ' . rupiah($d['tdisplin']) ?></td>
+						</tr>
+						<tr>
+							<td width="50%">TERLAMBAT </td>
+							<td align="right" width="50%">: <?= $d['status_displin'] ?> X</td>
+						</tr>
+						<tr>
+							<td width="50%">POTONGAN :</td>
+							<td align="right" width="50%">: <?= 'Rp.' . rupiah($d['status_displin'] * $d['pdisplin']) ?></td>
+						</tr>
+						<tr>
+							<td width="50%"></td>
+							<td align="right" width="50%">---------------------</td>
+						</tr>
+						<tr>
+							<td width="50%">GAJI DITERIMA :</td>
+							<td align="right" width="50%">: <?= 'Rp.' . rupiah($d['gapok'] - ($d['status_displin'] * $d['pdisplin']) + $tuk)  ?></td>
+						</tr>
+					</table>
+				</td>
+			</tr>
+		</table>
 
-</html>
+		<center><br>Padang,
+			<?php echo date('d-M-y') ?>
+			<br><br><br><br>
+			<u>(......................................)</u><br>
+			<b>Owner Toko</b>
+		</center>
+	</body>
+
+	</html>
+<?php $no++;
+} ?>
 <?php
 // $html = ob_get_contents();
 // ob_end_clean();
