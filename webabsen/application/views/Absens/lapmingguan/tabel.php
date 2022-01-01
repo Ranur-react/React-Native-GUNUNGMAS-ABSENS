2b5f9b403b5c3d09598734f3dfe3f0f1d90d90f5
Rahmat Nur <rahmatnur845@gmail.com>
2022-01-01 16:49:26 +0700

    jumlah hadir

--- webabsen/application/views/Absens/lapmingguan/tabel.php
+++ webabsen/application/views/Absens/lapmingguan/tabel.php
@@ -1,12 +1,14 @@
 <?php $no = 1;
-foreach ($dataVar as $d) { ?>
+foreach ($dataVar as $d) {
+	$jumlhaAlfaKotor = alfaHitung($d['rentangSet']) -$hari;
+	?>
 	<tr>
 		<td class="text-center" width="40px"><?= $no . '.'; ?></td>
 		<td><?= $d['nama_karyawan'] ?></td>
 		<td><?= $d['lokasi'] ?></td>
 		<td><?= $d['hadir'] ?></td>
 		<td><?= $d['sakit'] ?></td>
-		<td><?= $hari - ($d['sakit'] + $d['hadir']) ?></td>
+		<td><?= ($jumlhaAlfaKotor - ($d['hadir'] + $d['sakit'])) < 0 ? 0 : ($jumlhaAlfaKotor - ($d['hadir'] + $d['sakit']));?></td>
 		<td><?= $d['status_displin'] ?></td>
 	</tr>
 <?php $no++;
