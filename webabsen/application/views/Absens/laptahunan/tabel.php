84c6a74f36a72f53c79f13202396545e5bbe1dca
Rahmat Nur <rahmatnur845@gmail.com>
2022-01-01 23:41:22 +0700

    cetak lap

--- webabsen/application/views/Absens/laptahunan/tabel.php
+++ webabsen/application/views/Absens/laptahunan/tabel.php
@@ -1,5 +1,7 @@
 <?php $no = 1;
-foreach ($dataVar as $d) { ?>
+foreach ($dataVar as $d) {
+	$jumlhaAlfaKotor = 365;
+?>
 	<tr>
 		<td class="text-center" width="40px"><?= $no . '.'; ?></td>
 		<td><?= $d['nama_karyawan'] ?></td>
@@ -7,7 +9,8 @@ foreach ($dataVar as $d) { ?>
 		<td><?= $d['hadir'] ?></td>
 		<td><?= $d['sakit'] ?></td>
 		<td><?= number_format(($d['hadir'] / 365) * 100, 0) . "%"; ?></td>
-		<td><?= 365 - ($d['sakit'] + $d['hadir']) ?></td>
+		<td><?= ($jumlhaAlfaKotor - ($d['hadir'] + $d['sakit'])) < 0 ? 0 : ($jumlhaAlfaKotor - ($d['hadir'] + $d['sakit']));   ?></td>
+		<td><?= $d['sakit'] ?></td>
 		<td><?= $d['status_displin'] ?></td>
 	</tr>
 <?php $no++;
