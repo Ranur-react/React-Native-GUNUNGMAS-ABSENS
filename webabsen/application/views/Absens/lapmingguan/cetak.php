62d4e34f2d00fbbbfc574847c1f42af8593a9875
Rahmat Nur <rahmatnur845@gmail.com>
2022-01-02 00:29:02 +0700

    rentang set calling front

--- webabsen/application/views/Absens/lapmingguan/cetak.php
+++ webabsen/application/views/Absens/lapmingguan/cetak.php
@@ -1,67 +1,80 @@
-<?php //ob_start(); ?>
+<?php //ob_start(); 
+?>
 <!DOCTYPE html>
 
 <html>
+
 <head>
 	<title>Laporan Absensi Karyawan per Periode Tanggal</title>
 	<style>
+		table {
+			border-collapse: collapse;
+			table-layout: fixed;
+		}
 
- table {border-collapse:collapse; table-layout:fixed;}
- table td {word-wrap:break-word;text-align: left;}
-
- </style>
+		table td {
+			word-wrap: break-word;
+			text-align: left;
+		}
+	</style>
 </head>
+
 <body onload="window.print()">
 	<h1 align="center">Laporan Data Absensi Karyawan per Periode Tanggal
-		 <br>Konter Lorus Cellular</h1>
+		<br>Konter Lorus Cellular
+	</h1>
 	<h3 align="center">Kota Padang</h3>
 	<table align="center" width="60%" border="0">
-	
-	<tr>
-		<td width="20%">Parameter tanggal :</td>
-		<td align="right" width="15%"> <?= $awal; ?></td>
-		<td align="right" width="5%"> s/d</td>
-		<td align="right" width="10%"> <?= $akhir; ?> </td>
-		<td align="right" width="50%">  </td>
-	</tr>
-</table>
 
-<table align="center"  border="1">
-	<thead>
+		<tr>
+			<td width="20%">Parameter tanggal :</td>
+			<td align="right" width="15%"> <?= $awal; ?></td>
+			<td align="right" width="5%"> s/d</td>
+			<td align="right" width="10%"> <?= $akhir; ?> </td>
+			<td align="right" width="50%"> </td>
+		</tr>
+	</table>
 
-						<tr>
-							<th class="text-center" width="5%">No.</th>
-							<th>Nama Karyawan</th>
-							<th>Lokasi</th>
-							<th>Hadir</th>
-							<th>Sakit</th>
-							<th>Izin</th>
-						</tr>
-					</thead>
-					<tbody>
-						<?php $no = 1;
-						foreach ($data as $d) { ?>
-							<tr>
-								<td class="text-center" width="40px"><?= $no . '.'; ?></td>
-								<td><?= $d['nama_karyawan'] ?></td>
-								<td><?= $d['lokasi'] ?></td>
-								<td><?= $d['hadir'] ?></td>
-								<td><?= $d['sakit'] ?></td>
-								<td><?= $d['izin'] ?></td>
-							</tr>
-						<?php $no++;
-						} ?>
-					</tbody>
-	<tfoot>
-						
-					</tfoot>
-</table>
-<center><br>Padang,
-<?php echo date('d-M-y') ?>
-<br><br><br><br>
-<u>(......................................)</u><br>
-<b>Pemilik Toko</b></center>
+	<table align="center" border="1">
+		<thead>
+
+			<tr>
+				<th class="text-center" width="5%">No.</th>
+				<th>Nama Karyawan</th>
+				<th>Lokasi</th>
+				<th>Hadir</th>
+				<th>Sakit</th>
+				<th>Alfa</th>
+				<th>Terlambat</th>
+			</tr>
+		</thead>
+		<tbody>
+			<?php $no = 1;
+			foreach ($data as $d) { ?>
+				<tr>
+					<td class="text-center" width="40px"><?= $no . '.'; ?></td>
+					<td><?= $d['nama_karyawan'] ?></td>
+					<td><?= $d['lokasi'] ?></td>
+					<td><?= $d['hadir'] ?></td>
+					<td><?= $d['sakit'] ?></td>
+					<td><?= ($jumlhaAlfaKotor - ($d['hadir'] + $d['sakit'])) < 0 ? 0 : ($jumlhaAlfaKotor - ($d['hadir'] + $d['sakit'])); ?></td>
+					<td><?= $d['status_displin'] ?></td>
+				</tr>
+			<?php $no++;
+			} ?>
+		</tbody>
+		<tfoot>
+
+		</tfoot>
+	</table>
+	<center><br>Padang,
+		<?php echo date('d-M-y') ?>
+		<br><br><br><br>
+		<u>(......................................)</u><br>
+		<b>Pemilik Toko</b>
+	</center>
 </body>
+
 </html>
 <?php
 // $html = ob_get_contents();
