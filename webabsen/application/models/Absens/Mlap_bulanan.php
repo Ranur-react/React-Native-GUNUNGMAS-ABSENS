826b29d27164c3b0a2070b7c8764aea3892033e0
Rahmat Nur <rahmatnur845@gmail.com>
2022-01-27 03:26:40 +0700

    bulan

--- webabsen/application/models/Absens/Mlap_bulanan.php
+++ webabsen/application/models/Absens/Mlap_bulanan.php
@@ -23,7 +23,7 @@ class Mlap_bulanan extends CI_Model
 				GROUP BY `id_karyawan`;")->result_array();
 	}
 	
-	public function showsSlips($kode,$empId, $tahun)
+	public function showsSlips($kode,$empId, $thun)
 
 	{
 	$v=$kode;
@@ -31,30 +31,64 @@ class Mlap_bulanan extends CI_Model
 	$date = date_create('2020-12-01');
 	date_add($date, date_interval_create_from_date_string($a));
 	$d=date_format($date, 'Y-m');
-		return $this->db->query("SELECT `id_karyawan`,`nama_karyawan`, `lokasi`,`nama_jabatan`,
+		// return $this->db->query("SELECT `id_karyawan`,`nama_karyawan`, `lokasi`,`nama_jabatan`,
 				 
-				(SELECT COUNT(`status_kehadiran`) FROM `detail_jadwal` WHERE `id_karyawan`=`id_karyawan_detail` AND `status_kehadiran` != 'null'AND `status_kehadiran` != '0' )
-				 AS hadir,
+		// 		(SELECT COUNT(`status_kehadiran`) FROM `detail_jadwal` WHERE `id_karyawan`=`id_karyawan_detail` AND `status_kehadiran` != 'null'AND `status_kehadiran` != '0' )
+		// 		 AS hadir,
 
-				 (SELECT COUNT(`status_kehadiran`) FROM `detail_jadwal` WHERE `id_karyawan`=`id_karyawan_detail` AND `status_kehadiran` = 's')
-				 AS sakit,
+		// 		 (SELECT COUNT(`status_kehadiran`) FROM `detail_jadwal` WHERE `id_karyawan`=`id_karyawan_detail` AND `status_kehadiran` = 's')
+		// 		 AS sakit,
 				 
-				  (SELECT COUNT(`status_kehadiran`) FROM `detail_jadwal` WHERE `id_karyawan`=`id_karyawan_detail` AND `status_kehadiran` = 'i')
-				 AS izin,
-				 (SELECT COUNT(`status_displin`) FROM `detail_jadwal` WHERE `id_karyawan`=`id_karyawan_detail` AND `status_displin` != 'null' AND `status_displin` = '0')
-				 AS status_displin,
+		// 		  (SELECT COUNT(`status_kehadiran`) FROM `detail_jadwal` WHERE `id_karyawan`=`id_karyawan_detail` AND `status_kehadiran` = 'i')
+		// 		 AS izin,
+		// 		 (SELECT COUNT(`status_displin`) FROM `detail_jadwal` WHERE `id_karyawan`=`id_karyawan_detail` AND `status_displin` != 'null' AND `status_displin` = '0')
+		// 		 AS status_displin,
+		// 		 tanggal,
+		// 		 `rentangSet`,
+		// 		`tb_jabatan`.`gapok` AS gapok,
+		// 		`tb_jabatan`.`tunjangan_disiplin` AS tdisplin,
+		// 		`tb_jabatan`.`potongan_disiplin` AS pdisplin
+		// 		 FROM `jadwal_absen_karyawan` 
+		// 		JOIN  `detail_jadwal` ON `id_jadwal_detail`=`id_jadwal`
+		// 		JOIN `karyawan` ON `id_karyawan` =`id_karyawan_detail`
+		// 		JOIN `set_lokasi` ON `id_set_lokasi` =`id_lokasi_absensi`
+		// 		JOIN `tb_jabatan` ON `tb_jabatan`.`id_jabatan`=karyawan.`jabatan_id`
+		// 		WHERE  MONTH(tanggal)='$v' and  YEAR(tanggal)='$tahun' and id_karyawan='$empId'
+		// 		GROUP BY `id_karyawan`")->result_array();
+		return $this->db->query("SELECT `id_karyawan`,nama_karyawan,`lokasi`,
+attCount('1',
+STR_TO_DATE(CONCAT('$thun-',SUBSTRING(`rentangSet`, 1, 2),'-',SUBSTRING(`rentangSet`, 4, 2)),'%Y-%m-%d'),
+STR_TO_DATE(CONCAT('$thun-',SUBSTRING(`rentangSet`, 14, 2),'-',SUBSTRING(`rentangSet`, 17, 2)),'%Y-%m-%d'),
+id_karyawan) AS hadir,
+
+attCount('0',
+STR_TO_DATE(CONCAT('$thun-',SUBSTRING(`rentangSet`, 1, 2),'-',SUBSTRING(`rentangSet`, 4, 2)),'%Y-%m-%d'),
+STR_TO_DATE(CONCAT('$thun-',SUBSTRING(`rentangSet`, 14, 2),'-',SUBSTRING(`rentangSet`, 17, 2)),'%Y-%m-%d'),
+id_karyawan) AS alfa,
+
+attCount('s',
+STR_TO_DATE(CONCAT('$thun-',SUBSTRING(`rentangSet`, 1, 2),'-',SUBSTRING(`rentangSet`, 4, 2)),'%Y-%m-%d'),
+
+STR_TO_DATE(CONCAT('$thun-',SUBSTRING(`rentangSet`, 14, 2),'-',SUBSTRING(`rentangSet`, 17, 2)),'%Y-%m-%d'),
+id_karyawan) AS sakit,
+
+attCountDisplin('1',
+STR_TO_DATE(CONCAT('$thun-',SUBSTRING(`rentangSet`, 1, 2),'-',SUBSTRING(`rentangSet`, 4, 2)),'%Y-%m-%d'),
+
+STR_TO_DATE(CONCAT('$thun-',SUBSTRING(`rentangSet`, 14, 2),'-',SUBSTRING(`rentangSet`, 17, 2)),'%Y-%m-%d'),
+id_karyawan)  AS status_displin,
 				 tanggal,
-				 `rentangSet`,
+				`rentangSet`,
 				`tb_jabatan`.`gapok` AS gapok,
 				`tb_jabatan`.`tunjangan_disiplin` AS tdisplin,
 				`tb_jabatan`.`potongan_disiplin` AS pdisplin
-				 FROM `jadwal_absen_karyawan` 
-				JOIN  `detail_jadwal` ON `id_jadwal_detail`=`id_jadwal`
-				JOIN `karyawan` ON `id_karyawan` =`id_karyawan_detail`
-				JOIN `set_lokasi` ON `id_set_lokasi` =`id_lokasi_absensi`
-				JOIN `tb_jabatan` ON `tb_jabatan`.`id_jabatan`=karyawan.`jabatan_id`
-				WHERE  MONTH(tanggal)='$v' and  YEAR(tanggal)='$tahun' and id_karyawan='$empId'
-				GROUP BY `id_karyawan`")->result_array();
+FROM `jadwal_absen_karyawan` 
+JOIN  `detail_jadwal` ON `id_jadwal_detail`=`id_jadwal`
+JOIN `karyawan` ON `id_karyawan` =`id_karyawan_detail`
+JOIN `set_lokasi` ON `id_set_lokasi` =`id_lokasi_absensi`
+JOIN `tb_jabatan` ON `tb_jabatan`.`id_jabatan`=karyawan.`jabatan_id`
+WHERE `rentangSet` LIKE '%$v/%' AND `rentangSet` LIKE '%/$thun%' and id_karyawan='$empId'
+GROUP BY id_karyawan;")->result_array();
 	}
 	public function shows($kode)
 
