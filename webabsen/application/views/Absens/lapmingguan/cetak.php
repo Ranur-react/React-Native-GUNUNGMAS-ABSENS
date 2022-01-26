98683654e3661e76fe0e4866f688afc0d4b6ec21
Rahmat Nur <rahmatnur845@gmail.com>
2022-01-27 03:22:35 +0700

    bulan

--- webabsen/application/views/Absens/lapmingguan/cetak.php
+++ webabsen/application/views/Absens/lapmingguan/cetak.php
@@ -51,17 +51,17 @@
 		<tbody>
 			<?php $no = 1;
 			foreach ($data as $d) {
-				$dateMax= $akhir;
+				$dateMax = $akhir;
 				$jumlhaAlfaKotor = alfaHitungMingguan($d['rentangSet'], $dateMax);
 				$jumlhaAlfaKotorFull = alfaHitungBulanan($d['rentangSet']);
-				?>
+			?>
 				<tr>
 					<td class="text-center" width="40px"><?= $no . '.'; ?></td>
 					<td><?= $d['nama_karyawan'] ?></td>
 					<td><?= $d['lokasi'] ?></td>
-					<td><?= $d['hadir'] ?></td>
+					<td><?= $d['hadir'] + $d['status_displin'] ?></td>
 					<td><?= $d['sakit'] ?></td>
-					<td><?= ($jumlhaAlfaKotor - ($d['hadir'] + $d['sakit'])) < 0 ? 0 : ($jumlhaAlfaKotor - ($d['hadir'] + $d['sakit'])); ?></td>
+					<td><?= $d['alfa'] ?></td>
 					<td><?= $d['status_displin'] ?></td>
 				</tr>
 			<?php $no++;
